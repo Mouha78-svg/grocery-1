aa5a17a000a59d652c198db66da82d41e761f7d2
Hadydiop8 <108351337+Hadydiop8@users.noreply.github.com>
2023-06-16 01:28:53 +0100

    nouveau ajout
    PHD

---
+++ coobook/img.php
@@ -0,0 +1,71 @@
+
+ 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	 
+	 
+	<title></title>
+</head>
+<body>
+
+<section class="slider-wrapper">
+	<div class="slider-track">
+		 
+			<div class="slidee">
+				<img src="IMAGES/IMG1.jpg">
+			</div>
+
+			<div class="slidee">
+				<img src="IMAGES/IMG4.avif">
+			</div>
+
+			<div class="slidee">
+				<img src="IMAGES/IMG5.avif">
+			</div>
+
+			<div class="slidee">
+				<img src="IMAGES/IMG6.avif">
+			</div>
+
+			<div class="slidee">
+				<img src="images/IMG7.avif">
+			</div>
+
+			<div class="slide">
+				<img src="images/IMG8.avif">
+			</div>
+
+			<div class="slidee">
+				<img src="images/IMG9.avif">
+			</div>
+			
+	</div>
+	
+</section>
+
+
+ 
+<h2>ENSEMBLE, nous travaillons sur github</h2>
+<div class="trait"></div>
+ 
+
+    
+ 
+</div>
+
+<script src="slide/script.js"></script>
+
+
+
+</body>                                                                                                                                                                                                                                                                                                                                                                                                               
+
+                                                             
+
+ 
+
+</html>
